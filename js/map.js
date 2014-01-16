947706f75ac06f7b98cdd3f0b537458239e13339
Eric van Zanten <eric@bahai.us>
2014-01-16 12:08:46 -0600

    Ready to start pulling down the detailed aggregates

--- js/map.js
+++ js/map.js
@@ -6,6 +6,74 @@
     var endpoint = 'http://wopr.datamade.us';
     // TODO: ResponseView should be a wrapper for individual ChartViews and a
     // ChartView + a DataDetailView once you drill down into a dataset
+    function template_cache(tmpl_name, tmpl_data){
+        if ( !template_cache.tmpl_cache ) {
+            template_cache.tmpl_cache = {};
+        }
+
+        if ( ! template_cache.tmpl_cache[tmpl_name] ) {
+            var tmpl_dir = '/js/templates';
+            var tmpl_url = tmpl_dir + '/' + tmpl_name + '.html';
+
+            var tmpl_string;
+            $.ajax({
+                url: tmpl_url,
+                method: 'GET',
+                async: false,
+                success: function(data) {
+                    tmpl_string = data;
+                }
+            });
+
+            template_cache.tmpl_cache[tmpl_name] = _.template(tmpl_string);
+        }
+
+        return template_cache.tmpl_cache[tmpl_name](tmpl_data);
+    }
+    var ErrorView = Backbone.View.extend({
+        initialize: function(){
+            this.render()
+        },
+        render: function(){
+            this.$el.html(template_cache('modalTemplate', this.model));
+            this.$el.modal();
+            return this;
+        }
+    });
+    var ChartView = Backbone.View.extend({
+        render: function(){
+            this.$el.html(template_cache('chartTemplate', this.model));
+            return this;
+        },
+        addData: function(data){
+            var el = this.model['el'];
+            var name = this.model['objects']['dataset_name'];
+            var agg = this.attributes.agg
+            var iteration = this.attributes.iteration
+            ChartHelper.create(el, name, 'City of Chicago', agg, data, iteration);
+        }
+    });
+    var ExploreView = Backbone.View.extend({
+        events: {
+            'click .refine': 'refineQuery'
+        },
+        initialize: function(){
+            // Maybe call the /api/fields/<dataset>/ to get a list of fields
+            this.render();
+        },
+        render: function(){
+            this.$el.html(template_cache('exploreForm', {query: this.attributes.base_query}));
+            return this;
+        },
+        refineQuery: function(e){
+            var refined = this.$el.find('textarea').val();
+            this.attributes.parent.remove();
+            var refined_query = parseParams(refined);
+            var query = $.extend(this.attributes.base_query, refined_query);
+            // TODO: Probably need a RefineView that loads the data from a different endpoint
+            // First, need to make sure the endpoint will work that way.
+        },
+    });
     var ResponseView = Backbone.View.extend({
         events: {
             'click .data-download': 'fetchDownload',
@@ -14,9 +82,13 @@
         render: function(){
             var self = this;
             this.query = this.attributes.query;
+            if (typeof this.explore !== 'undefined'){
+                this.explore.remove();
+            }
+            this.$el.empty();
+            this.charts = {};
             $.when(this.getResults()).then(function(resp){
                 self.$el.spin(false);
-                self.$el.html('');
                 results = resp.objects;
                 $.each(results, function(i, obj){
                     var el = obj.objects[0].dataset_name;
@@ -26,34 +98,47 @@
                         chart_id: chart_id,
                         objects: obj
                     }
-                    var chartTpl = new EJS({url: self.template});
-                    self.$el.append(chartTpl.render(item));
+                    var chart = new ChartView({
+                        model: item,
+                        attributes: {
+                            agg: self.query['agg'],
+                            iteration: i
+                        }
+                    });
+                    self.$el.append(chart.render().el);
                     var data = [];
                     $.each(obj.objects, function(i, o){
                         data.push([moment(o.group).unix()*1000, o.count]);
                     });
-                    ChartHelper.create(el, obj.dataset_name, 'City of Chicago', agg, data, i);
+                    chart.addData(data);
+                    self.charts[el] = chart;
                 });
             }).fail(function(resp){
-                this.$el.spin(false);
-                var error = {
-                    header: 'Woops!',
-                    body: resp['responseJSON']['meta']['message'],
-                }
-                var errortpl = new EJS({url: 'js/templates/modalTemplate.ejs'})
-                $('#errorModal').html(errortpl.render(error));
-                $('#errorModal').modal();
+                new ErrorView({el: '#errorModal', model: resp});
             });
         },
         fetchDownload: function(e){
-            var dataset = $(e.target).attr('id').split('-')[0];
-            var datatype = $(e.target).attr('id').split('-')[1];
-            var url = endpoint + '/api/master/?' + $.param(this.query) + '&dataset_name=' + dataset + '&datatype=' + datatype;
+            this.query['dataset'] = $(e.target).attr('id').split('-')[0];
+            this.query['datatype'] = $(e.target).attr('id').split('-')[1];
+            var url = endpoint + '/api/master/?' + $.param(this.query);
             window.open(url, '_blank');
         },
-        exploreDataset: function(){
-            var dataset = $(e.target).attr('id').split('-')[0];
-            console.log('exploreit');
+        exploreDataset: function(e){
+            var self = this;
+            this.query['dataset'] = $(e.target).attr('id').split('-')[0];
+            this.query['datatype'] = 'json';
+            $.each(this.charts, function(key,chart){
+                if (key != self.query.dataset){
+                    chart.remove();
+                }
+            });
+            this.explore = new ExploreView({
+                el: '#explore',
+                attributes: {
+                    base_query: this.query,
+                    parent: self
+                }
+            });
         },
         getResults: function(){
             var self = this;
@@ -62,23 +147,23 @@
                 dataType: 'json',
                 data: self.query
             });
-        },
-        parseParams: function(query){
-            var re = /([^&=]+)=?([^&]*)/g;
-            var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
-            var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
-            var params = {}, e;
-            while ( e = re.exec(query) ) {
-                var k = decode( e[1] ), v = decode( e[2] );
-                if (k.substring(k.length - 2) === '[]') {
-                    k = k.substring(0, k.length - 2);
-                    (params[k] || (params[k] = [])).push(v);
-                }
-                else params[k] = v;
-            }
-            return params;
         }
     });
+    function parseParams(query){
+        var re = /([^&=]+)=?([^&]*)/g;
+        var decodeRE = /\+/g;  // Regex for replacing addition symbol with a space
+        var decode = function (str) {return decodeURIComponent( str.replace(decodeRE, " ") );};
+        var params = {}, e;
+        while ( e = re.exec(query) ) {
+            var k = decode( e[1] ), v = decode( e[2] );
+            if (k.substring(k.length - 2) === '[]') {
+                k = k.substring(0, k.length - 2);
+                (params[k] || (params[k] = [])).push(v);
+            }
+            else params[k] = v;
+        }
+        return params;
+    }
     $(document).ready(function(){
         resize_page();
         window.onresize = function(event){
